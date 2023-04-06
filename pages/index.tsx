ca9f94bd5ce6245a2a29f722feb035ed87cef340
Aurelijus Banelis <aurelijus@banelis.lt>
2023-04-06 22:44:28 +0300

    Small style improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <Head>
@@ -12,21 +14,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <ul>
-          <li>
-            <Link href="/posts/first-post">First post</Link>
-          </li>
-        </ul>
-        <Image
-          src="/next.svg"
-          alt=""
-          width={1000}
-          height={1000}
-        />
+      <main className="flex flex-col h-screen">
+      <header className="bg-gray-800 text-white px-4 py-2">
+        <h1 className="text-lg font-bold">My Website</h1>
+      </header>
+      <main className="flex-1 p-4">
+        <h2 className="text-xl font-bold mb-4">Welcome to my website!</h2>
+        <p className="text-gray-600">
+          <Link href="/posts/first-post">First post</Link>
+        </p>
+      </main>
+      <footer className="bg-gray-800 text-white px-4 py-2">
+        <p className="text-center">Copyright © {currentYear}</p>
+      </footer>
       </main>
     </>
   )
